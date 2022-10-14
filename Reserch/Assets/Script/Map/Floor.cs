8b9a627f13e4c0fa76768efe0063c4b29340ea7f
Kissy <yuutomain20@gmail.com>
2022-10-14 16:41:29 +0900

    色変えメソッド作成

--- Reserch/Assets/Script/Map/Floor.cs
+++ Reserch/Assets/Script/Map/Floor.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Floor : MonoBehaviour
 {
+    SpriteRenderer spRen;
+
     GameObject ObjectOnFloor;//
+
+    [SerializeField] Sprite RedFloorSprite;
+    [SerializeField] Sprite BlueFloorSprite;
 
     public enum floorColor
     {
@@ -17,17 +23,32 @@ public class Floor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        spRen = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 
     public floorColor getColor()
     {
         return this.color;
+    }
+
+    publicÅ@void changeColor()
+    {
+        if (color == floorColor.Red)
+        {
+            color = floorColor.Blue;
+            spRen.sprite = BlueFloorSprite;
+        }
+        else
+        {
+            color = floorColor.Red;
+            spRen.sprite = RedFloorSprite;
+        }
     }
 }
